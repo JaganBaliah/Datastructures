5af75d02281c6df2adb6e9a6a9840acc2193eef7
Jagan Baliah <jagan.baliah@aa.com>
2022-05-25 13:51:21 -0500

    Included few more problems and solutions

--- src/org/jagan/sorting/CountingSortUtil.java
+++ src/org/jagan/sorting/CountingSortUtil.java
@@ -2,6 +2,8 @@ package org.jagan.sorting;
 
 import java.util.Arrays;
 
+import org.junit.jupiter.api.Assertions;
+
 public class CountingSortUtil {
 
 	public static void main(String[] args) {
@@ -31,5 +33,41 @@ public class CountingSortUtil {
 		}
 		System.out.println(Arrays.toString(outputArr));
 	}
-	
+
+	private static void intArr() {
+		int[] arr = {12, 11, 12, 13, 7, 5, 6, 7};
+		System.out.println(Arrays.toString(arr));
+		int len = arr.length;
+		int max = arr[0];
+		for (int val : arr) {
+			if (val > max) max = val;
+		}
+		int cLen = max + 1;
+		int[] cArr = new int[cLen];
+		for (int i = 0; i < len; i++) cArr[arr[i]]++;
+		for (int i = 1; i < cLen; i++) cArr[i] += cArr[i-1];
+		int[] tArr = new int[len];
+		for (int i = len - 1; i >= 0; i--) tArr[--cArr[arr[i]]] = arr[i];
+		for (int i = 0; i < len; i++) arr[i] = tArr[i];
+
+		System.out.println(Arrays.toString(arr));
+		Assertions.assertEquals("[5, 6, 7, 7, 11, 12, 12, 13]", Arrays.toString(arr));
+	}
+
+	private static void charArr() {
+		char[] arr = {'a', 'l', 'p', 'h', 'a', 'b', 'e', 't'};
+		System.out.println(Arrays.toString(arr));
+		int len = arr.length;
+
+		int cLen = 26;
+		int[] cArr = new int[cLen];
+		for (int i = 0; i < len; i++) cArr[arr[i] - 'a']++;
+		for (int i = 1; i < cLen; i++) cArr[i] += cArr[i-1];
+		char[] tArr = new char[len];
+		for (int i = len - 1; i >= 0; i--) tArr[--cArr[arr[i] - 'a']] = arr[i];
+		for (int i = 0; i < len; i++) arr[i] = tArr[i];
+
+		System.out.println(Arrays.toString(arr));
+		Assertions.assertEquals("[a, a, b, e, h, l, p, t]", Arrays.toString(arr));
+	}
 }
